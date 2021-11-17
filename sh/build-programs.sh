54f4b30bad072eabd43186352593190b60aac55f
Thorsten Lorenz <thlorenz@gmx.de>
2021-11-17 14:10:29 -0600

    sh: building only metaplex core programs

--- sh/build-programs.sh
+++ sh/build-programs.sh
@@ -1,7 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ROOT=$DIR/..
-SMART_CONTRACTS=$ROOT/../../metaplex/smart-contracts
 
 programs=(
   auction/program
@@ -15,7 +14,3 @@ for prog in "${programs[@]}"; do
   cd "$ROOT/rust/$prog"
   cargo build-bpf
 done
-
-echo "Building nft-packs"  
-cd "$SMART_CONTRACTS/nft-packs"
-cargo build-bpf
