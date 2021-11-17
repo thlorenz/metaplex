e6e8acb1cc20f65b113d93645596f06ab95f7bb9
Thorsten Lorenz <thlorenz@gmx.de>
2021-11-17 10:38:12 -0600

    sh: full setup of test-validator including determistic program ids

--- sh/deploy-programs.sh
+++ sh/deploy-programs.sh
@@ -3,6 +3,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT=$DIR/..
 SMART_CONTRACTS=$ROOT/../../metaplex/smart-contracts
 
+KEYPAIRS=$DIR/keypairs
+CONFIG_PATH=$DIR/config.yml
+
 deployables=(
   metaplex_auction
   metaplex_token_metadata
@@ -14,10 +17,12 @@ for dep in "${deployables[@]}"; do
   echo
   echo "Deploying $dep"  
   SOFILE="$ROOT/rust/target/deploy/$dep.so"
-  solana program deploy $SOFILE
+  PROGRAMID="$KEYPAIRS/$dep-keypair.json"
+  solana -C $CONFIG_PATH program deploy --program-id $PROGRAMID $SOFILE
 done
 
 
-echo "Deploying nft-packs"  
+echo "Deploying metaplex nft-packs"  
 SOFILE="$SMART_CONTRACTS/target/deploy/metaplex_nft_packs.so"
-solana program deploy $SOFILE
+PROGRAMID="$KEYPAIRS/metaplex_nft_packs-keypair.json"
+solana -C $CONFIG_PATH program deploy --program-id $PROGRAMID $SOFILE
